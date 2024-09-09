15e4df844f797aa644aaaa5bf97be0247945d9bc
PETER ABBOTT <abbottsayshi@gmail.com>
2024-09-09 15:34:37 +0200

    Initial Commit

---
+++ Conditional Rendering/Conditional_RenderingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Conditional_RenderingApp.swift
+//  Conditional Rendering
+//
+//  Created by Peter Abbott on 09/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct Conditional_RenderingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
