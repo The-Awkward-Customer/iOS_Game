23e47772284709296c86b0553c77a0f7e03a2a0f
PETER ABBOTT <abbottsayshi@gmail.com>
2025-01-03 15:24:03 +0100

    Dumping

---
+++ Bumble Boogie/Legacy/LegacyComponents/ResetBtn.swift
@@ -0,0 +1,30 @@
+////
+////  ResetBtn.swift
+////  Conditional Rendering
+////
+////  Created by Peter Abbott on 11/09/2024.
+////
+//
+//import SwiftUI
+//
+//
+//struct ResetBtn: View {
+//    
+//    @ObservedObject var gameState: GameState  // Reference the game state
+//
+//    
+//    var body: some View {
+//        Button (action: {
+//            gameState.hardReset()
+//        }) {
+//            Text("reset game")
+//                .foregroundColor(Color.gray)
+//                .fontWeight(.bold)
+//        }
+//        
+//    }
+//}
+//
+//#Preview {
+//    ResetBtn(gameState: GameState())
+//}
